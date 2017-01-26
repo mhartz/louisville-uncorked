c06beb3375d96860cb5425f3b6a62b525c8f5a03
mhartz <mshartz5@gmail.com>
2017-01-25 23:33:42 -0500

    revert constant to show registration until AM

--- config/constants.php
+++ config/constants.php
@@ -7,7 +7,7 @@
  */
 return [
     'EVENT_ACTIVE'=> true,
-    'EVENT_REGISTRATION_OPEN' => true,
+    'EVENT_REGISTRATION_OPEN' => false,
     'EVENT_TEAM_TABLE' => 'teams_2017_winter',
     'EVENT_PARTICIANT_TABLE' => 'participants_2017_winter'
 ];
