db9f53cf5353a5a9d986bd09e3ca1e6b388dd810
mhartz <mshartz5@gmail.com>
2017-06-05 17:29:30 -0400

    changed out the admin layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Admin Panel</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
@@ -13,6 +13,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}" />
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
     <style>
@@ -46,10 +47,11 @@
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                </ul>
-
+                @if(!Auth::guest())
+                    <ul class="nav navbar-nav">
+                        <li><a href="{{ url('/admin/editTeams') }}">Edit Teams</a></li>
+                    </ul>
+                @endif
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
@@ -76,6 +78,7 @@
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src="{{ URL::asset('js/participants-list.js') }}"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
--- resources/views/pages/teams/participants-list.blade.php
+++ resources/views/pages/teams/participants-list.blade.php
@@ -1,14 +1,16 @@
-@extends('layout')
+@extends('layouts.app')
 
 @section('content')
 
     <div class="wrapper">
         <h1 class="main-heading">Participants page</h1>
-            <table class="edit-teams-table data-table mt-40 mb-40">
+        <div class="table-responsive">
+            <table class="edit-teams-table table mt-40 mb-40">
                 <tr>
                     <th>ID</th>
                     <th>Name</th>
                     <th>Team Name</th>
+                    <th>Edit Menu</th>
                 </tr>
 
                 @foreach ($participants as $participant)
@@ -17,23 +19,24 @@
                             {{ csrf_field() }}
                             <tr>
                                 <td><input participant-id-attr-{{ $participant->id }} type="hidden" class="form-control" name="participantId" value="{{ $participant->id }}">{{ $participant->id }}</td>
-                                <td><input first-name-attr-{{ $participant->id }} type="text" class="form-control" name="firstName" value="{{ $participant->first_name }}" edit-team-input disabled> <input last-name-attr-{{ $participant->id }} type="text" class="form-control" name="lastName" value="{{ $participant->last_name }}" edit-team-input disabled></td>
+                                <td class="names"><input first-name-attr-{{ $participant->id }} type="text" class="form-control" name="firstName" value="{{ $participant->first_name }}" edit-team-input disabled> <input last-name-attr-{{ $participant->id }} type="text" class="form-control" name="lastName" value="{{ $participant->last_name }}" edit-team-input disabled></td>
                                 @foreach ($teams as $team)
                                     @if ($participant->team_id == $team->id)
                                         <td>
                                             <input team-name-attr-{{ $participant->id }} team-id-name-{{ $team->id }} type="text" class="form-control" name="teamName" value="{{ $team->team_name }}" edit-team-input disabled>
-                                            <input team-id-attr-{{ $participant->id }} type="text" class="form-control" name="teamId" value="{{ $team->id }}" edit-team-input disabled hidden>
+                                            <input team-id-attr-{{ $participant->id }} type="hidden" class="form-control" name="teamId" value="{{ $team->id }}" edit-team-input disabled>
                                         </td>
                                     @endif
                                 @endforeach
                                 <td>
                                     <a class="edit-participant-button btn btn-submit" data-id="{{ $participant->id }}" data-edit-state="false">Edit</a>
-                                    <a class="cancel-participant-button btn btn-submit" data-id="{{ $participant->id }}" data-edit-state="false">Cancel</a>
+                                    <a class="cancel-participant-button btn btn-submit outline" data-id="{{ $participant->id }}" data-edit-state="false">Cancel</a>
                                 </td>
                             </tr>
                         </form>
                     @endif
                 @endforeach
             </table>
+        </div>
     </div>
 @stop
