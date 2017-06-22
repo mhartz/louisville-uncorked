aa7de5479532af8fe6d9f6c9d3ab06f4ecf26841
mhartz <mshartz5@gmail.com>
2017-06-22 12:03:04 -0400

    closed summer 2017 registration

--- config/constants.php
+++ config/constants.php
@@ -1,13 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mshartz5
- * Date: 1/25/17
- * Time: 10:18 PM
- */
+
 return [
     'EVENT_ACTIVE'=> true,
-    'EVENT_REGISTRATION_OPEN' => true,
+    'EVENT_REGISTRATION_OPEN' => false,
     'EVENT_TEAM_TABLE' => 'teams_2017_winter',
     'EVENT_PARTICIANT_TABLE' => 'participants_2017_winter'
 ];
