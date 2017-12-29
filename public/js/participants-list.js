9a5eb6b0a562a093c66a1a85005c269f59b24109
mhartz <mshartz5@gmail.com>
2017-12-29 00:15:45 -0500

    Added show/hide for all elements on edit teams page

--- public/js/participants-list.js
+++ public/js/participants-list.js
@@ -1,88 +1,49 @@
 var ParticipantsList = {
     init: function() {
-        var participantIdData,
-            firstName,
-            lastName,
-            teamName,
-            teamId,
-            token;
 
-        $('.edit-participant-button').on('click', function() {
-            participantIdData = this.dataset.id;
+        $('.edit-team-button').on('click', function() {
+            var self = this;
+            ParticipantsList.editTeam(self);
 
-            if(this.dataset.editState === "false") {
-                $(this).html('Save');
-                $('input[first-name-attr-' + participantIdData + ']').prop('disabled', '');
-                $('input[last-name-attr-' + participantIdData + ']').prop('disabled', '');
-                $('input[team-name-attr-' + participantIdData + ']').prop('disabled', '');
-                $('input[team-id-attr-' + participantIdData + ']').prop('disabled', '');
-                this.dataset.editState = "true";
-            }
-            else {
-                var parentId = 'form[data-parent-id="' + participantIdData + '"]';
-                token = $(parentId).next().val();
-
-                firstName = $('input[first-name-attr-' + participantIdData + ']').val();
-                lastName = $('input[last-name-attr-' + participantIdData + ']').val();
-                teamName = $('input[team-name-attr-' + participantIdData + ']').val();
-                teamId = $('input[team-id-attr-' + participantIdData + ']').val();
-
-                console.log(teamId);
-
-                $.ajax({
-                    method: 'POST',
-                    url: 'editTeams',
-                    data: {
-                        '_token': token,
-                        'participantId': participantIdData,
-                        'firstName': firstName,
-                        'lastName': lastName,
-                        'teamName': teamName,
-                        'teamId' : teamId
-                    },
-                    success: function(data) {
-                        console.log(data);
-                        $('input[team-id-name-' + teamId + ']').each(function() {
-                            $(this).val(teamName);
-                        });
-                    },
-                    error: function() {
-                        console.log('hit was error');
-                    }
-                });
-                $('input[first-name-attr-' + participantIdData + ']').prop('disabled', 'disabled');
-                $('input[last-name-attr-' + participantIdData + ']').prop('disabled', 'disabled');
-                $('input[team-name-attr-' + participantIdData + ']').prop('disabled', 'disabled');
-                $('input[team-id-attr-' + participantIdData + ']').prop('disabled', 'disabled');
-                $(this).html('Edit');
-                this.dataset.editState = "false";
-
-            }
-
-            // would like to refresh the data automatically (reactively) if possible
-            // might need to look into a simple Vue implementation for this
-
-            // Also need to add validation
-
-            // Need to refactor into methods
+                // $.ajax({
+                //     method: 'POST',
+                //     url: 'editTeams',
+                //     data: {
+                //         '_token': token,
+                //         'participantId': participantIdData,
+                //         'firstName': firstName,
+                //         'lastName': lastName,
+                //         'teamName': teamName,
+                //         'teamId' : teamId
+                //     },
+                //     success: function(data) {
+                //         console.log(data);
+                //         $('input[team-id-name-' + teamId + ']').each(function() {
+                //             $(this).val(teamName);
+                //         });
+                //     },
+                //     error: function() {
+                //         console.log('hit was error');
+                //     }
+                // });
         });
 
-        $('.cancel-participant-button').on('click', function() {
-            var participantIdCancelData = this.dataset.id;
-
-            $('input[first-name-attr-' + participantIdCancelData + ']').prop('disabled', 'disabled');
-            $('input[last-name-attr-' + participantIdCancelData + ']').prop('disabled', 'disabled');
-            $('input[team-name-attr-' + participantIdCancelData + ']').prop('disabled', 'disabled');
-            $('input[team-id-attr-' + participantIdCancelData + ']').prop('disabled', 'disabled');
-            $('.edit-participant-button[data-id="' + participantIdCancelData + '"]').html('Edit');
-        });
-
-        $('.add-participant-button').on('click', function() {
-            
+        $('.cancel-save-team').on('click', function() {
+            var self = this;
+            ParticipantsList.cancelEditTeam(self);
         });
     },
-    addTeammate: function() {
-        
+    editTeam: function(self) {
+        var teamIdData = self.dataset.id;
+
+        $('.editable-value').removeClass('hidden');
+        $('.team-static-text').addClass('hidden');
+    },
+    cancelEditTeam: function(self) {
+        var teamIdData = self.dataset.id;
+
+        $('.editable-value').addClass('hidden');
+        $('.team-static-text').removeClass('hidden');
     }
 };
 
--- resources/views/pages/teams/participants-list.blade.php
+++ resources/views/pages/teams/participants-list.blade.php
@@ -8,40 +8,6 @@
             <a href="{{ url('admin/participantListPrintByTeam') }}" target="_blank" class="btn btn-primary">Printable List by Team</a>
             <a href="{{ url('admin/participantListPrintByName') }}" target="_blank" class="btn btn-primary">Printable List by Last Name</a>
             <table class="edit-teams-table table mt-40 mb-40">
-                {{--<tr>--}}
-                    {{--<th>ID</th>--}}
-                    {{--<th>Name</th>--}}
-                    {{--<th>Team Name</th>--}}
-                    {{--<th>Edit Menu</th>--}}
-                {{--</tr>--}}
-
-                {{--@foreach ($participants as $participant)--}}
-                    {{--@if ($participant->active == 1)--}}
-                        {{--<form class="form-horizontal" method="POST" action="./editTeams" role="form" data-parent-id="{{ $participant->id }}">--}}
-                            {{--{{ csrf_field() }}--}}
-                            {{--<tr>--}}
-                                {{--<td><input participant-id-attr-{{ $participant->id }} type="hidden" class="form-control" name="participantId" value="{{ $participant->id }}">{{ $participant->id }}</td>--}}
-                                {{--<td class="names"><input first-name-attr-{{ $participant->id }} type="text" class="form-control" name="firstName" value="{{ $participant->first_name }}" edit-team-input disabled> <input last-name-attr-{{ $participant->id }} type="text" class="form-control" name="lastName" value="{{ $participant->last_name }}" edit-team-input disabled></td>--}}
-                                {{--@foreach ($teams as $team)--}}
-                                    {{--@if ($participant->team_id == $team->id)--}}
-                                        {{--<td>--}}
-                                            {{--<input team-name-attr-{{ $participant->id }} team-id-name-{{ $team->id }} type="text" class="form-control" name="teamName" value="{{ $team->team_name }}" edit-team-input disabled>--}}
-                                            {{--<input team-id-attr-{{ $participant->id }} type="hidden" class="form-control" name="teamId" value="{{ $team->id }}" edit-team-input disabled>--}}
-                                        {{--</td>--}}
-                                    {{--@endif--}}
-                                {{--@endforeach--}}
-                                {{--<td>--}}
-                                    {{--@if ($count <= 2)--}}
-                                        {{--<a class="add-participant-button btn btn-submit" data-id="{{ $team->id }}" data-edit-state="false">+ Add</a>--}}
-                                    {{--@endif--}}
-                                    {{--<a class="edit-participant-button btn btn-submit" data-id="{{ $participant->id }}" data-edit-state="false">Edit</a>--}}
-                                    {{--<a class="cancel-participant-button btn btn-submit outline" data-id="{{ $participant->id }}" data-edit-state="false">Cancel</a>--}}
-                                {{--</td>--}}
-                            {{--</tr>--}}
-                        {{--</form>--}}
-                    {{--@endif--}}
-                {{--@endforeach--}}
-
                 <tr>
                     <th>ID</th>
                     <th>Team Name</th>
@@ -52,16 +18,24 @@
                 @foreach ($teams as $count=>$team)
                     <tr>
                         <td>{{ $count }}</td>
-                        <td>{{ $team->team_name }}</td>
-                        <td>
+                        <td class="team-names">
+                            <div class="team-static-text">
+                                {{ $team->team_name }}
+                            </div>
+                            <div class="team-editable editable-value hidden">
+                                <input team-id-attr-{{ $team->id }} type="hidden" class="form-control" name="teamId" value="{{ $team->id }}">
+                                <p><input team-name-attr-{{ $team->id }} type="text" class="form-control" name="teamName" value="{{ $team->team_name }}"></p>
+                            </div>
+                        </td>
+                        <td class="names">
                             @php ($participantCount = 0)
                             @foreach ($participants as $participant)
                                 @if ($participant->team_id == $team->id)
-                                    <div id="team-uneditable">
+                                    <div class="team-static-text">
                                         <p>{{ $participant->first_name }} {{  $participant->last_name }}</p>
                                     </div>
 
-                                    <div id="team-editable">
+                                    <div class="team-editable editable-value hidden">
                                         <input participant-id-attr-{{ $participant->id }} type="hidden" class="form-control" name="participantId" value="{{ $participant->id }}">
                                         <p><input first-name-attr-{{ $participant->id }} type="text" class="form-control" name="firstName" value="{{ $participant->first_name }}"> <input last-name-attr-{{ $participant->id }} type="text" class="form-control" name="lastName" value="{{ $participant->last_name }}"></p>
                                     </div>
@@ -69,22 +43,26 @@
                                 @endif
                             @endforeach
                             @if ($participantCount <= 1)
-                                <p><input type="text" class="form-control" name="firstName" value="Teammate 2 First Name"> <input type="text" class="form-control" name="lastName" value="Teammate 2 Last Name"></p>
+                                <div class="new-team-members editable-value hidden">
+                                    <p><input type="text" class="form-control" name="firstName" value="First Name"> <input type="text" class="form-control" name="lastName" value="Last Name"></p>
+                                </div>
                             @endif
 
                             @if ($participantCount <= 2)
-                                <p><input type="text" class="form-control" name="firstName" value="Teammate 3 First Name"> <input type="text" class="form-control" name="lastName" value="Teammate 3 Last Name"></p>
+                                <div class="new-team-members editable-value hidden">
+                                    <p><input type="text" class="form-control" name="firstName" value="First Name"> <input type="text" class="form-control" name="lastName" value="Last Name"></p>
+                                </div>
                             @endif
                         </td>
                         <td data-edit-state="false">
-                            <div id="edit-team-section">
-                                <button id="edit-team" class="btn btn-submit" data-team-id="{{ $team->id }}">Edit</button>
-                                <button id="remove-team" class="btn btn-submit outline" data-team-id="{{ $team->id }}">Delete</button>
+                            <div class="edit-team-section team-static-text">
+                                <button class="edit-team-button btn btn-submit" data-id="{{ $team->id }}">Edit</button>
+                                <button class="remove-team btn btn-submit outline" data-team-id="{{ $team->id }}">Delete</button>
                             </div>
 
-                            <div id="save-cancel-team-section">
-                                <button id="save-team" class="btn btn-submit" data-team-id="{{ $team->id }}">Save</button>
-                                <button id="cancel-save-team" class="btn btn-submit outline" data-team-id="{{ $team->id }}">Cancel</button>
+                            <div class="save-cancel-team-section editable-value hidden">
+                                <button class="save-team btn btn-submit" data-team-id="{{ $team->id }}">Save</button>
+                                <button class="cancel-save-team btn btn-submit outline" data-team-id="{{ $team->id }}">Cancel</button>
                             </div>
                         </td>
                     </tr>
