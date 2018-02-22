910090de5c2bca595609ac15820ece283bb0596b
mhartz <mshartz5@gmail.com>
2018-02-22 08:59:13 -0500

    closing the winter 2018 event

--- config/constants.php
+++ config/constants.php
@@ -3,7 +3,7 @@
 return [
     'EVENT_ACTIVE'=> true,
     'EVENT_REGISTRATION_OPEN' => false,
-    'EVENT_THIRD_PARTY_REGISTRATION_OPEN' => true,
+    'EVENT_THIRD_PARTY_REGISTRATION_OPEN' => false,
     'EVENT_TEAM_TABLE' => 'teams_2017_winter',
     'EVENT_PARTICIANT_TABLE' => 'participants_2017_winter'
 ];
