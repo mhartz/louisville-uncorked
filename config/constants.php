c501112dcf1f137d0a0df13cff8ac6c797d8e151
mhartz <mshartz5@gmail.com>
2018-08-25 15:42:22 -0400

    Opened registration for the Fall 2018 event

--- config/constants.php
+++ config/constants.php
@@ -4,7 +4,7 @@ return [
     'EVENT_ACTIVE'=> true,
     'EVENT_REGISTRATION_UPCOMING' => true,
     'EVENT_REGISTRATION_OPEN' => false,
-    'EVENT_THIRD_PARTY_REGISTRATION_OPEN' => false,
+    'EVENT_THIRD_PARTY_REGISTRATION_OPEN' => true,
     'EVENT_TEAM_TABLE' => 'teams_2017_winter',
     'EVENT_PARTICIANT_TABLE' => 'participants_2017_winter'
 ];
