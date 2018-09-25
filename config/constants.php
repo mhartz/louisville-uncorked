236e1843f67f65f0fc07371938baa391ba742958
mhartz <mshartz5@gmail.com>
2018-09-25 12:37:22 -0400

    closing summer 2018 event registration

--- config/constants.php
+++ config/constants.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'EVENT_ACTIVE'=> true,
-    'EVENT_REGISTRATION_UPCOMING' => true,
+    'EVENT_ACTIVE'=> false,
+    'EVENT_REGISTRATION_UPCOMING' => false,
     'EVENT_REGISTRATION_OPEN' => false,
-    'EVENT_THIRD_PARTY_REGISTRATION_OPEN' => true,
+    'EVENT_THIRD_PARTY_REGISTRATION_OPEN' => false,
     'EVENT_TEAM_TABLE' => 'teams_2017_winter',
     'EVENT_PARTICIANT_TABLE' => 'participants_2017_winter'
 ];
